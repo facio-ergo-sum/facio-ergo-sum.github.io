9b4e078860e0416239185e855f911717632ed866
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-08-28 13:27:44 +0200

    Add link

--- src/Pitch.tsx
+++ src/Pitch.tsx
@@ -1,5 +1,6 @@
 import {MutableRefObject} from "react";
 import {PitchSection} from "./Pitch.style.tsx";
+import Hyperlink from "./Hyperlink.tsx";
 
 
 interface PitchProps {
@@ -13,9 +14,11 @@ export default function Pitch(props: PitchProps) {
             <h3>Welcome to Facio Ergo Sum</h3>
             <p>
                 At Facio Ergo Sum, we believe that great software begins with simplicity and a commitment to quality. As
-                creators of "Create Issue Branch" — a powerful GitHub app designed to streamline your development
-                workflow — we focus on crafting tools that make a difference. Our passion lies in building minimalistic
-                yet effective software solutions that elevate your projects without the unnecessary complexity.<br/>
+                creators of <Hyperlink href="https://github.com/robvanderleek/create-issue-branch">"Create Issue
+                Branch"</Hyperlink> — a powerful GitHub app designed to
+                streamline your development workflow — we focus on crafting tools that make a difference. Our passion
+                lies in building minimalistic yet effective software solutions that elevate your projects without the
+                unnecessary complexity.<br/>
                 Discover a team of developers dedicated to enhancing your coding experience, one small but impactful
                 project at a time.
             </p>
